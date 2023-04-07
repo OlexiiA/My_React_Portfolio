360365db9918b0e55bbf59c5e7629fb43d41029e
OlexiiA <incretiblealex@gmail.com>
2023-04-07 18:05:56 +0300

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,19 @@ const Hero = () => {
   
   const { hero } = content;
 
-  const handleDownload = () => {
-    fetch("https://github.com/OlexiiA/My_React_Portfolio/blob/6e61ac04fced7202954319f6a38bb7aa0660948d/src/assets/Olexii_CV.pdf")
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", "Olexii_CV.pdf");
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-      });
-  };
+  // const handleDownload = () => {
+  //   fetch("https://github.com/OlexiiA/My_React_Portfolio/blob/6e61ac04fced7202954319f6a38bb7aa0660948d/src/assets/Olexii_CV.pdf")
+  //     .then((response) => response.blob())
+  //     .then((blob) => {
+  //       const url = window.URL.createObjectURL(new Blob([blob]));
+  //       const link = document.createElement("a");
+  //       link.href = url;
+  //       link.setAttribute("download", "Olexii_CV.pdf");
+  //       document.body.appendChild(link);
+  //       link.click();
+  //       link.parentNode.removeChild(link);
+  //     });
+  // };
 
   return <section id="home">
     <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
@@ -36,9 +36,9 @@ const Hero = () => {
         <h2>{hero.title}</h2>
         <br />
         <div className="flex justify-end">
-          {/* <a href="https://t.me/OlexiiA" alt="..." className="btn">{hero.btnText}</a> */}
+          <a href="https://t.me/OlexiiA" alt="..." className="btn">{hero.btnText}</a>
          
-          <button onClick={handleDownload} className="btn hover:bg-dark_primary hover:text-[#EAF2FA]">Download CV</button>
+          {/* <button onClick={handleDownload} className="btn hover:bg-dark_primary hover:text-[#EAF2FA]">Download CV</button> */}
           
         </div>
         <div className="flex flex-col gap-10 mt-10">
