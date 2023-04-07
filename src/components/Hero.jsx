0d902b867afbfb39b7f3b7bd9db976462aaec3bd
OlexiiA <incretiblealex@gmail.com>
2023-04-07 17:58:44 +0300

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   const { hero } = content;
 
   const handleDownload = () => {
-    fetch("./src/assets/Olexii_CV.pdf")
+    fetch("https://github.com/OlexiiA/My_React_Portfolio/blob/6e61ac04fced7202954319f6a38bb7aa0660948d/src/assets/Olexii_CV.pdf")
       .then((response) => response.blob())
       .then((blob) => {
         const url = window.URL.createObjectURL(new Blob([blob]));
